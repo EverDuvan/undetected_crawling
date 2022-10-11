08042c4d13c22b682d671766f157086b8f454c38
SanVargas <Santi0000@hotmail.com>
2022-10-11 16:34:29 -0500

    Update SeleniumController.py

--- controller/SeleniumController.py
+++ controller/SeleniumController.py
@@ -1,30 +1,26 @@
 import undetected_chromedriver as uc
-#from controller.ProxyController import get_proxy_random
-#from xvfbwrapper import Xvfb
+from controller.ProxyController import get_proxy_random
+from xvfbwrapper import Xvfb
 import time
 
 
 def start_driver():
-
     driver = None
-
     try:
-       # vdisplay = Xvfb(width=800, height=1280)
-       # vdisplay.start()
+        vdisplay = Xvfb(width=800, height=1280)
+        vdisplay.start()
         options = uc.ChromeOptions()
         options.add_argument(
             '--no-first-run --no-service-autorun --password-store=basic')
         options.add_argument(f'--disable-gpu')
         options.add_argument(f'--no-sandbox')
         options.add_argument(f'--disable-dev-shm-usage')
-       # options.add_argument('--proxy-server=' + str(get_proxy_random()))
-        CHROME_DRIVER_PATH = 'C:\\Users\\Userr\\spring-workspace\\chromedriver.exe'
+        options.add_argument('--proxy-server=' + str(get_proxy_random()))
+        CHROME_DRIVER_PATH = '/usr/bin/chromedriver'
         driver = uc.Chrome(executable_path=CHROME_DRIVER_PATH,
                            options=options, headless=False)
     except Exception as e:
-        driver = None
-        print(f'error en start_driver(): {e}')
-
+        print(e)
     return driver
 
 
