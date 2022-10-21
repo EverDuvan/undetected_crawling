f242b18b7ec5535c92ac4596c7ff87d2b64a3fcc
SanVargas <Santi0000@hotmail.com>
2022-10-21 10:20:52 -0500

    Update ServiceCrawling.py

--- service_selenium/ServiceCrawling.py
+++ service_selenium/ServiceCrawling.py
@@ -1,10 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
-from controller.ProductController import *
 from controller.DBController import get_retailer_crawling
 from service_selenium.SamsClub import start_crawling
 from controller.DataframeController import SetDateFrame
-
-p = ProductController()
 
 
 def start_scrap_crawling():
@@ -14,7 +11,6 @@ def start_scrap_crawling():
         dataframe = get_retailer_crawling("samsclub")
         for i, df in dataframe.iterrows():
             executor.submit(start_crawling(df))
-
-        SetDateFrame(p.product, 'product_details', 'psql_write').send_df_append
+            
     except Exception as e:
         print(f'error en start_scrap_crawling in ServiceCrawling.py: {e}')
